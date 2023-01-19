9d343bbd634814300ae62d03bd7d9944d0e70799
fruitbowl7 <iriehill812@riversideunified.org>
2023-01-19 11:02:41 -0800

    Adding scripts

--- First Game/Assets/Scripts/GameManager.cs
+++ First Game/Assets/Scripts/GameManager.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DetectCollisions : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(other.gameObject);
+        Destroy(this.gameObject);
     }
 }
